8a3821abff964e0ff3954d8b2238a896716bcd72
yeonsuBaek <soothbugger@gmail.com>
2023-11-25 13:30:28 +0900

    #1 💄 Style a main page

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,8 +1,55 @@
+import { Add } from "@mui/icons-material"
+import {
+  Avatar,
+  Button,
+  Container,
+  Divider,
+  List,
+  ListItem,
+  ListItemAvatar,
+  ListItemText,
+} from "@mui/material"
+import ImageIcon from "@mui/icons-material/Image"
+
 const Home = () => {
   return (
-    <main>
-      <div>Hello</div>
-    </main>
+    <Container maxWidth="sm">
+      <main>
+        <h1 style={{ fontSize: "24px", margin: "20px 0px" }}>You can do it!</h1>
+        <Button
+          variant="contained"
+          startIcon={<Add />}
+          style={{ textTransform: "none", marginBottom: "16px" }}
+        >
+          Add OGSM
+        </Button>
+        <List
+          sx={{
+            width: "100%",
+            maxWidth: 360,
+            bgcolor: "background.paper",
+          }}
+        >
+          <ListItem>
+            <ListItemAvatar>
+              <Avatar>
+                <ImageIcon />
+              </Avatar>
+            </ListItemAvatar>
+            <ListItemText primary="Photos" secondary="D-10" />
+          </ListItem>
+          <Divider component="li" />
+          <ListItem>
+            <ListItemAvatar>
+              <Avatar>
+                <ImageIcon />
+              </Avatar>
+            </ListItemAvatar>
+            <ListItemText primary="Work" secondary="D-20" />
+          </ListItem>
+        </List>
+      </main>
+    </Container>
   )
 }
 
